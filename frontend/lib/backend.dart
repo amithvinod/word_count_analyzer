8eacc5431b2b45a22e38b9766f81ab495af96eff
amithvinod <amithvinod8@gmail.com>
2024-11-09 14:49:28 +0530

    backend url change

--- frontend/lib/backend.dart
+++ frontend/lib/backend.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = '[your-backend-url]/analyze';
+  static const String baseUrl = 'http://10.0.2.2:8000/analyze';
 
   Future<List<Map<String, dynamic>>> fetchWordFrequency(
       String url, int topN) async {
